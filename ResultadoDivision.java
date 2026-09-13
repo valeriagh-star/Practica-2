b87eb39b0c204a2336b183ea10c099af2e37b2fd
valeriagh-star <valeriagh@ciencias.unam.mx>
2026-09-13 13:44:44 -0600

    Se agrega ResultadoDivision.java

---
+++ ResultadoDivision.java
@@ -0,0 +1,11 @@
+public class ResultadoDivision {
+    int llavePromovida;
+    NodoArbolB hijoIzquierdo;
+    NodoArbolB hijoDerecho;
+
+    public ResultadoDivision(int llavePromovida, NodoArbolB hijoIzquierdo, NodoArbolB hijoDerecho) {
+        this.llavePromovida = llavePromovida;
+        this.hijoIzquierdo = hijoIzquierdo;
+        this.hijoDerecho = hijoDerecho;
+    }
+}
